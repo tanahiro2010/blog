a913e9844378d0fd52b281c2c8d3391d01ee3709
tanahiro2010 <herentongkegu087@gmail.com>
2026-05-29 16:50:52 +0900

    fix(auth): safe_userの取得方法を修正

--- app/controllers/api/auth_controller.rb
+++ app/controllers/api/auth_controller.rb
@@ -1,7 +1,7 @@
 class Api::AuthController < ApiApplicationController
   def index
     if @current_user
-      safe_user = `@current_user.as_json.except`("password_hashed")
+      safe_user = @current_user.as_json.except("password_hashed")
       render json: ApiApplicationHelper::Response.ok(data: safe_user)
     else
       render json: ApiApplicationHelper::Response.unauthorized, status: :unauthorized
