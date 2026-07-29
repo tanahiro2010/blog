4493f840b19a23f5ecfd8da6d512cb25f3aebc0f
tanahiro2010 <herentongkegu087@gmail.com>
2026-07-29 12:17:47 +0900

    feat(core_ext): Integerにweekやyearなどの時間ヘルパー関数を追加

--- app/lib/core_ext/integer.rb
+++ app/lib/core_ext/integer.rb
@@ -4,4 +4,20 @@ class Integer
   def week
     self * 7.day
   end
+
+  def month
+    self.week * 4
+  end
+
+  def year
+    self.month * 12
+  end
+
+  def quarter
+    self.month * 3
+  end
+
+  def half_year
+    self.month * 6
+  end
 end
